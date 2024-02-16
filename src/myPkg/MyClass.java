a55fb7f040d901ae7b1749ef88cddfa727d4ea03
Bhushan <dateybhushan@yahoo.com>
2024-02-16 18:49:21 +0530

    third commit

--- src/myPkg/MyClass.java
+++ src/myPkg/MyClass.java
@@ -17,6 +17,21 @@ public class MyClass {
 
 		System.out.println("Wassup World");
 
+
+
+		System.out.println("1");
+
+		System.out.println("2");
+
+		System.out.println("3");
+
+		System.out.println("4");
+
+		
+		System.out.println("5");
+
+		System.out.println("6");
+
 	}
 
 }
