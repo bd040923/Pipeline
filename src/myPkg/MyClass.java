ec2cfdccecbb8c30830cb9f06500892023e26a8e
Bhushan <dateybhushan@yahoo.com>
2024-02-16 22:06:41 +0530

    new features

--- src/myPkg/MyClass.java
+++ src/myPkg/MyClass.java
@@ -32,6 +32,16 @@ public class MyClass {
 
 		System.out.println("6");
 
+
+		System.out.println("7");
+
+		System.out.println("8");
+		
+		System.out.println("9");
+
+		System.out.println("10");
+
+
 	}
 
 }
